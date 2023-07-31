da0bccdbdb2c25a022ee85e8c4ca43af6ff787a7
Grace Zhang <nyd8pd@virginia.edu>
2023-07-31 11:50:35 -0400

    updated file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+
+    name='bookloverPackage',
+
+    version='1.0.0',
+
+    url='https://github.com/graceziqi/booklover',
+
+    author='Grace Zhang',
+
+    author_email='ziqia58@gmail.com',
+
+    description='Description of booklover package',
+
+    packages=find_packages(),    
+)
